b99c3c4752397d316a09202ee5bd4194c73bc000
Icloud-del <73659181+Icloud-del@users.noreply.github.com>
2020-10-29 21:49:17 +0530

    Update and rename com.antivirus to com.antivirus.py

---
+++ com.antivirus.py
@@ -0,0 +1,14 @@
+
+import os.path
+
+if os.path.isfile('virus.txt'):
+    print ("Virus detected")
+else:
+    print ("not a virus")
+
+
+
+if os.path.isfile('h.txt'):
+    print ("Virus detected")
+else:
+    print ("not a virus")
